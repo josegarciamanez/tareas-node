a22f095207e6de60fda5e2258b1a7b79616f9ab3
Jose Garcia <josegarciamanez@gmail.com>
2018-04-16 23:47:40 +0200

    inicio DB

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // const argv = require('yargs').argv
 const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0]
 
 switch (comando) {
     case 'crear':
-        console.log('Crear tarea por hacer')
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break
     case 'listar':
         console.log('Mostrar todas las tareas por hacer')
